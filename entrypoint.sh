490b0c7299a1db0d9f1641b716a734a0dea63d49
Puneet Arora <puneetarora.er@gmail.com>
2023-01-16 13:38:56 -0800

    fixing

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,12 +33,11 @@ content_dir=$(get_content_dir "${INPUT_CONTENT_DIR}" "${INPUT_WORKDIR}")
 
 # TODO: Here we should run different package for all the different kind of checks
 
-run_language_checks() {
+
+
   vale sync --config="${content_dir}/.vale.ini"
   vale "$content_dir" --config="${content_dir}/.vale.ini" --output="$vale_template" >> "$vale_output"
-}
 
-run_language_checks
 
 < ek_vale_output.txt reviewdog -efm="%f:%l:%c: %m" \
       -name="EkLineReviewer" \
