3fe1ad6b77ab006e21611b39bfa95ce87209b907
Puneet Arora <puneetarora.er@gmail.com>
2023-01-16 14:35:25 -0800

    fixing again

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,10 @@ get_content_dir() {
   fi
 }
 
-#run_language_checks() {
-#
-#}
+run_language_checks() {
+  vale sync
+  vale --output="$vale_template" >> "${vale_output}"
+}
 
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
@@ -32,13 +33,11 @@ content_dir=$(get_content_dir "${INPUT_CONTENT_DIR}" "${INPUT_WORKDIR}")
 pwd
 
 
-#run_language_checks
-vale sync --config="${content_dir}.vale.ini"
-  vale "$content_dir" --config="${content_dir}.vale.ini" --output="$vale_template" >> ek_vale_output.txt
+run_language_checks
 
-cat ek_vale_output.txt
+cat $vale_output
 
-cat ek_vale_output.txt | reviewdog -efm="%f:%l:%c: %m" \
+cat $vale_output | reviewdog -efm="%f:%l:%c: %m" \
       -name="EkLineReviewer" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
