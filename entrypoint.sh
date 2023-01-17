0edc6254ee0bf129d3eb36c1824cb1f4f320fb09
Puneet Arora <puneetarora.er@gmail.com>
2023-01-17 11:06:16 -0800

    ekline check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e -o xtrace
 
+ek_check_zip="ek_check.zip"
+
+if [ -n "${GITHUB_WORKSPACE}" ] ; then
+  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
+fi
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+echo "input_ek_token=${INPUT_EK_TOKEN}"
+echo "input_work_dir=${INPUT_WORKDIR}"
+echo "input_content_dir=${INPUT_CONTENT_DIR}"
+
 get_content_dir() {
   if [ -z "$1" ]; then
     echo "$2"
@@ -9,33 +21,30 @@ get_content_dir() {
   fi
 }
 
-run_language_checks() {
-  vale sync
-  vale ./ --output="$vale_template" >> $vale_output
+content_dir=$(get_content_dir "${INPUT_CONTENT_DIR}" "${INPUT_WORKDIR}")
+echo "content_dir=${content_dir}"
+
+
+setup_vale_files(){
+  unzip $ek_check_zip
 }
 
+run_language_checks() {
+  vale_template="/files/vale/rdjsonl.tmpl"
+  vale_output="ek_vale_output.txt"
 
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
-  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
-fi
-
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-echo "ek_token=${INPUT_EK_TOKEN}"
-echo "content_dir=${INPUT_CONTENT_DIR}"
-echo "work_dir=${INPUT_WORKDIR}"
-
-vale_template="/files/vale/rdjsonl.tmpl"
-vale_output="ek_vale_output.txt"
-
-content_dir=$(get_content_dir "${INPUT_CONTENT_DIR}" "${INPUT_WORKDIR}")
+  setup_vale_files
+  vale sync
+  vale "$content_dir" --output="$vale_template" >> $vale_output
+}
 
 pwd
 
+run_language_checks "$content_dir"
 
-run_language_checks
+which $vale_output
 
-cat $vale_output
+more $vale_output
 
 cat $vale_output | reviewdog -efm="%f:%l:%c: %m" \
       -name="EkLineReviewer" \
