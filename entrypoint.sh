dcc345058659e3c8a7553b29bb77ff7d8d5a71c9
Puneet Arora <puneetarora.er@gmail.com>
2023-02-08 12:58:59 -0800

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,17 +18,17 @@ get_content_dir() {
 
 content_dir=$(get_content_dir "${INPUT_CONTENT_DIR}" "${INPUT_WORKDIR}")
 
-setup_vale_files(){
+#setup_vale_files(){
 #  wget -q "https://storage.googleapis.com/ekline-config/${INPUT_EK_TOKEN}/config.zip"
 #  unzip -q config.zip
-}
+#}
 
 vale_template="/files/vale/rdjsonl.tmpl"
 vale_output="${INPUT_WORKDIR}/ek_vale_output.txt"
 
 run_language_checks() {
   touch "$vale_output"
-  setup_vale_files
+#  setup_vale_files
   vale sync
   vale "$1" --output="$vale_template" --no-exit >> "$vale_output"
 }
