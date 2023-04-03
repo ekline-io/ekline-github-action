cdbbaeaa6e1e251701ae94fcf852bb33f2887c35
Puneet Arora <puneetarora.er@gmail.com>
2023-04-02 22:39:56 -0700

    adding gitlab config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 set -e
 
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
-  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
+if [ "$GITLAB_CI" = "true" ]; then
+  input_workspace="$CI_PROJECT_DIR"
+  rd_api_token="$INPUT_GITLAB_TOKEN"
+  disable_suggestions="--no-suggestion"
+elif [ "$GITHUB_ACTIONS" = "true" ]; then
+  input_workspace="$GITHUB_WORKSPACE"
+  rd_api_token="$INPUT_GITHUB_TOKEN"
+  disable_suggestions=""
+else
+  echo "Not running in GitLab CI or GitHub Actions"
+  exit 1
 fi
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+if [ -n "${input_workspace}" ] ; then
+  cd "${input_workspace}/${INPUT_WORKDIR}" || exit
+  git config --global --add safe.directory "${input_workspace}" || exit 1
+fi
+
+export REVIEWDOG_GITLAB_API_TOKEN="${rd_api_token}"
+export REVIEWDOG_GITHUB_API_TOKEN="${rd_api_token}"
 
 output="ekOutput.jsonl"
-ekline -cd "${INPUT_CONTENT_DIR}" -et "${INPUT_EK_TOKEN}"  -o "${output}"
+ekline -cd "${INPUT_CONTENT_DIR}" -et "${INPUT_EK_TOKEN}"  -o "${output}" "${disable_suggestions}"
 
 < "$output" reviewdog -f="rdjsonl" \
-      -name="EkLine" \
-      -reporter="${INPUT_REPORTER}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+  -name="EkLine" \
+  -reporter="${INPUT_REPORTER}" \
+  -filter-mode="${INPUT_FILTER_MODE}" \
+  ${INPUT_REVIEWDOG_FLAGS}
