1a41fcd1114d59b3a86b8afaa214821735a0a74d
Puneet Arora <puneet@ekline.io>
2023-10-31 00:09:17 -0700

    updating endtrypoints

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 set -e
 
+setGithubPullRequestId() {
+  is_pull_request="$(echo "$GITHUB_REF" | awk -F / '{print $2}')"
+  if [ "${is_pull_request}" = "pull" ]; then
+    pull_request_id="$(echo "$GITHUB_REF" | awk -F / '{print $3}')"
+  else
+    pull_request_id=""
+  fi
+}
+
 if [ "$GITLAB_CI" = "true" ]; then
   input_workspace="$CI_PROJECT_DIR"
   rd_api_token="$INPUT_GITLAB_TOKEN"
   disable_suggestions="--no-suggestion"
+  ci_platform="gitlab"
+  git_repository_id="$CI_PROJECT_ID"
+  pull_request_id="$CI_MERGE_REQUEST_IID"
+  workflow_run_id="$CI_PIPELINE_ID"
+  git_user_id="$GITLAB_USER_ID"
 elif [ "$GITHUB_ACTIONS" = "true" ]; then
   input_workspace="$GITHUB_WORKSPACE"
   rd_api_token="$INPUT_GITHUB_TOKEN"
   disable_suggestions=""
+  ci_platform="github"
+  git_repository_id="$GITHUB_REPOSITORY_ID"
+  setGithubPullRequestId
+  workflow_run_id="$GITHUB_RUN_ID"
+  git_user_id="$GITHUB_ACTOR_ID"
 else
   echo "Not running in GitLab CI or GitHub Actions"
   exit 1
@@ -21,6 +40,11 @@ fi
 
 export REVIEWDOG_GITLAB_API_TOKEN="${rd_api_token}"
 export REVIEWDOG_GITHUB_API_TOKEN="${rd_api_token}"
+export CI_PLATFORM="${ci_platform}"
+export GIT_REPOSITORY_ID="${git_repository_id}"
+export PULL_REQUEST_ID="${pull_request_id}"
+export WORKFLOW_RUN_ID="${workflow_run_id}"
+export GIT_USER_ID="${git_user_id}"
 
 output="ekOutput.jsonl"
 ekline -cd "${INPUT_CONTENT_DIR}" -et "${INPUT_EK_TOKEN}"  -o "${output}" -i "${INPUT_IGNORE_RULE}" "${disable_suggestions}"
